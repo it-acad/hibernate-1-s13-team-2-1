53426cfe4ceee7d6863b07db913862c0a56ef9d9
Bohdan Martyniuk <bo.martynyuk@gmail.com>
2020-10-24 19:42:35 +0300

    implemented all methods in Task and State classes

--- src/main/java/com/softserve/itacademy/model/State.java
+++ src/main/java/com/softserve/itacademy/model/State.java
@@ -1,5 +1,29 @@
 package com.softserve.itacademy.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "states")
 public class State {
- //TODO
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z0-9\\-\\_\\ ]{1,20}$")
+    private String name;
+
+    @OneToMany(mappedBy = "state")
+    private List<Task> tasks;
+
 }
--- src/main/java/com/softserve/itacademy/model/Task.java
+++ src/main/java/com/softserve/itacademy/model/Task.java
@@ -1,5 +1,34 @@
 package com.softserve.itacademy.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
 public class Task {
-//
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    @Pattern(regexp = "^.{3,200}$")
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @ManyToOne
+    @JoinColumn(name = "state_id", nullable = false)
+    private State state;
+
+    @ManyToOne
+    @JoinColumn(name = "todo_id", nullable = false)
+    private ToDo toDo;
+
 }
